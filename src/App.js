f359fa20d32247cb7372dabac618ff1c62d05af1
AngieV <angiev@me.com>
2022-03-01 23:17:38 -0600

    add Navbar, Raise state to show counters in Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        counters: [
+            { id: 1, value: 4 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
+        ]
+    };
+
+    handleIncrement = (counter) => {
+        //use the spread operator to clone the counters
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+    };
+
+    handleDecrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value--;
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+
+    //create a new array without a given counter, and let react update the state 
+    deleteCounter = (counterId) => {
+        //get all the counters except the one with the given ID
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        //display new array using the above counters const
+        //this.setState({ counters:counters}) key & value are the same so can be written as simply {counters}
+        this.setState({ counters })
+    };
+
+    render() {
+        return (
+            // show the total number of counters, filtered by those with a value >0
+            <React.Fragment >
+                <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+                <main className="container" >
+                    <Counters
+                        counters={this.state.counters}
+                        onDelete={this.deleteCounter}
+                        onReset={this.handleReset}
+                        onIncrement={this.handleIncrement}
+                        onDecrement={this.handleDecrement}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,63 +3,19 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id:1, value:4 },
-            { id:2, value:0 },
-            { id:3, value:0 },
-            { id:4, value:0 },
-        ]
-     };
-
-       handleIncrement = (counter) => {
-         //use the spread operator to clone the counters
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        this.setState({ counters });
-    }; 
-
-     handleDecrement = counter =>{
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value--;
-        this.setState({ counters });
-     }; 
-
-     handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0; 
-            return c;
-        });
-        this.setState({ counters });
-     };
-         
-
-    //create a new array without a given counter, and let react update the state 
-     deleteCounter = (counterId) => {
-         //get all the counters except the one with the given ID
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        //display new array using the above counters const
-        //this.setState({ counters:counters}) key & value are the same so can be written as simply {counters}
-        this.setState({ counters })
-     };
-
     render() { 
         return (
             <div>
-                {this.state.counters.map(counter => (
+                {this.props.counters.map(counter => (
                     <Counter 
-                    key={counter.id} 
-                    onDelete={this.deleteCounter} 
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                    counter={counter} 
+                        key={counter.id} 
+                        onDelete={this.props.onDelete} 
+                        onIncrement={this.props.onIncrement}
+                        onDecrement={this.props.onDecrement}
+                        counter={counter} 
                     />
                 ))}
-               <button onClick={this.handleReset} className="btn-primary btn-sm m-2">RESET</button>
+               <button onClick={this.props.onReset} className="btn-primary btn-sm m-2">RESET</button>
 
             </div>
         );
